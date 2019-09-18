21f45e7dae958c90eac9d3094e17b91fd2870b99
Alison1301 <55442615+Alison1301@users.noreply.github.com>
2019-09-18 08:47:36 +0200

    Update SnakeSegment.cs

--- SimpleGameEngine/SimpleGameEngine/SnakeSegment.cs
+++ SimpleGameEngine/SimpleGameEngine/SnakeSegment.cs
@@ -7,35 +7,23 @@ public class SnakeSegment{
     private char character; //the character representing this snake segment visually
 
     public SnakeSegment(int x, int y, char character){
-        /*  ------------------------------------------
-            1.1
-            ------------------------------------------   
-            Initialize the x, y and character fields 
-            using the values passed to the constructor
-        */
+
+        this.x = x;
+        this.y = y;
+        this.character=character;
     }
 
     public int X {
         get {return x;}
         set {
-            /*  ------------------------------------------
-                1.2
-                ------------------------------------------
-                Before overwriting x with the new value,
-                set prevX to the current value of x
-            */
+            x = prevX;
         }
     }
 
     public int Y {
         get {return y;}
         set {
-            /*  ------------------------------------------
-                1.3
-                ------------------------------------------
-                Before overwriting y with the new value,
-                set prevY to the current value of y
-            */
+            y = prevY;
         }
     }
 
